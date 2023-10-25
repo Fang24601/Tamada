34df27add722ce03b76660db72fb9292f3ccad35
maxs2001 <msteinbach99@gmail.com>
2023-10-25 14:21:08 -0700

    Added Unity Project

---
+++ Tamada Decentrium/Assets/Scripts/Controllers/UI/PetUIController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PetUIController : MonoBehaviour
+{
+    public Image foodImage, happinessImage;
+    public static PetUIController instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else Debug.LogWarning("More than one PetUIController in the Scene");
+    }
+
+    public void UpdateImages(int food, int happiness)
+    {
+        this.foodImage.fillAmount = (float)food / 100;
+        this.happinessImage.fillAmount = (float)happiness / 100;
+    }
+}
